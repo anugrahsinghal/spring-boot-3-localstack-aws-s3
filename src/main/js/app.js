01b41cb7dd7e6fad86db85f90b6fc67e5502296f
Anugrah Singhal <anugrahsinghal1@gmail.com>
2022-11-06 23:40:04 +0530

    feat: wiring up all components

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,86 +1,132 @@
-const React = require('react'); (1)
-const ReactDOM = require('react-dom'); (2)
-const client = require('./client'); (3)
+import {UploadForm} from "./uploadForm";
+import {FileWithLinksList, PdfRender} from "./fileWithLinksList";
+import axios from 'axios';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { fileWithLinks: [] };
+        this.state = {fileWithLinks: []};
     }
 
     componentDidMount() {
-        client({ method: 'GET', path: '/links' }).done(response => {
-            this.setState({ fileWithLinks: response.fileWithLinks });
+        console.log("component mounted");
+        const url = 'http://localhost:8080/links';
+        const config = {};
+        axios.get(url, config).then((response) => {
+            console.log(response.data);
+            console.log("fileWithLinks", response.data.fileWithLinks)
+            this.setState({fileWithLinks: response.data.fileWithLinks});
         });
     }
 
     render() {
         return (
             <div>
-                {/* <Form /> */}
-                <FileWithLinksList fileWithLinks={this.state.fileWithLinks} />
+                <UploadForm/>
+                <hr/>
+                <FileWithLinksList
+                    fileWithLinks={this.state.fileWithLinks}
+                />
+                <hr/>
+                <PdfRender/>
             </div>
         )
     }
 }
 
-class Form extends React.Component {
-    render() {
-        <div>
-            <form method="POST" enctype="multipart/form-data" action="/upload">
-                <table>
-                    <tr>
-                        <td>File to upload:</td>
-                        <td><input id="file" type="file" name="file" onchange="validateFileSizeAndEnableUpload()" accept=".pdf" /></td>
-                        {/* <td><input id="file" type="file" name="file" onchange="validateFileSizeAndEnableUpload()" accept=".pdf" /></td> */}
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><input type="submit" value="Upload" id="upload-button" /></td>
-                    </tr>
-                </table>
-            </form>
-        </div>
-    }
-}
-
-class FileWithLinksList extends React.Component {
-    render() {
-        const employees = this.props.fileWithLinks.map(fileWithLink =>
-            <FileWithLink key={fileWithLink.fileLink} fileWithLink={fileWithLink} />
-        );
-        return (
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Original Name</th>
-                        <th>Link</th>
-                        <th>Preview</th>
-                    </tr>
-                    {employees}
-                </tbody>
-            </table>
-        )
-    }
-}
-
-class FileWithLink extends React.Component {
-    render() {
-        return (
-            <tr>
-                <td>{this.props.fileWithLink.fileName}</td>
-                <td>{this.props.fileWithLink.fileLink}</td>
-                <td>{this.props.employee.description}
-                    <embed src={this.props.employee.description} width="100" height="100" alt="pdf"
-                        pluginspage="http://www.adobe.com/products/acrobat/readstep2.html"></embed>
-                </td>
-            </tr>
-        )
-    }
-}
 
 ReactDOM.render(
-    <App />,
+    <App/>
+    ,
     document.getElementById('react')
 )
+
+// class UploadForm extends React.Component
+// {
+//     constructor(props) {
+//         super(props);
+//         this.state = {value: ''};
+//
+//         this.handleChange = this.handleChange.bind(this);
+//         // this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+//
+//     handleChange(event) {
+//         const fileElement = event.target;
+//         const ONE_MEGABYTE = 1024;
+//         // check file selected
+//         if (fileElement.files.length > 0) {
+//             const fileSize = fileElement.files.item(0).size;
+//             const sizeInMb = Math.round((fileSize / ONE_MEGABYTE));
+//             console.log(sizeInMb)
+//             // The size of the file should be <= 1MB
+//             if (sizeInMb < ONE_MEGABYTE) {
+//                 if (document.getElementById('upload-button').disabled === true) {
+//                     document.getElementById('upload-button').disabled = false;
+//                 }
+//             } else {
+//                 document.getElementById('upload-button').disabled = true;
+//                 alert("File too Big, please select a file less than 1MB");
+//             }
+//         }
+//         this.setState({value: event.target.value});
+//     }
+//
+//     // handleSubmit(event) {
+//     //     alert('A name was submitted: ' + this.state.value);
+//     //     event.preventDefault();
+//     // }
+//
+//     render() {
+//         return (
+//             <form method="POST" encType="multipart/form-data" action="/upload">
+//                 <table>
+//                     <tr>
+//                         <td>
+//                             {/*<label>*/}
+//                             Upload File:
+//                         </td>
+//                         <td>
+//                             <input id="file" type="file" name="file" accept=".pdf" value={this.state.value}
+//                                    onChange={this.handleChange}/>
+//                         </td>
+//                         {/*</label>*/}
+//                     </tr>
+//                     <tr>
+//                         <td></td>
+//                         <td>
+//                             <input type="submit" value="Upload" id="upload-button"/>
+//                         </td>
+//                     </tr>
+//                 </table>
+//             </form>
+//         );
+//     }
+// }
+
+
+// class Form extends React.Component {
+//     render() {
+//         return (<div>
+//             <form method="POST" encType="multipart/form-data" action="/upload">
+//                 <table>
+//                     <tr>
+//                         <td>File to upload:</td>
+//                         {/*<td><input id="file" type="file" name="file" onChange=validateFileSizeAndEnableUpload*/}
+//                         {/*           accept=".pdf"/></td>*/}
+//                         {/* <td><input id="file" type="file" name="file" onchange="validateFileSizeAndEnableUpload()" accept=".pdf" /></td> */}
+//                     </tr>
+//                     <tr>
+//                         <td></td>
+//                         <td><input type="submit" value="Upload" id="upload-button"/></td>
+//                     </tr>
+//                 </table>
+//             </form>
+//         </div>)
+//     }
+// }
