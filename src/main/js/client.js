f7875587831ddee91fecd72c1b9c5f511ed162ca
anugrahsinghal <anugrahsinghal1@gmail.com>
2022-11-06 15:33:11 +0000

    chore: add table css and client for rest calls

---
+++ src/main/js/client.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const rest = require('rest');
+const defaultRequest = require('rest/interceptor/defaultRequest');
+const mime = require('rest/interceptor/mime');
+const errorCode = require('rest/interceptor/errorCode');
+const baseRegistry = require('rest/mime/registry');
+
+const registry = baseRegistry.child();
+
+registry.register('application/hal+json', require('rest/mime/type/application/hal'));
+registry.register('application/json', require('rest/mime/type/application/json'));
+
+module.exports = rest
+		.wrap(mime, { registry: registry })
+		.wrap(errorCode)
+		.wrap(defaultRequest, { headers: { 'Accept': 'application/json' }});
