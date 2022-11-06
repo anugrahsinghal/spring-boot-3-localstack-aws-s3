2bbf2a85a9b5d0dc8e261e3a7af30292832255a2
Anugrah Singhal <anugrahsinghal1@gmail.com>
2022-11-06 23:41:31 +0530

    chore: organize controllers

--- src/main/java/com/netcracker/utility/controller/FileController.java
+++ src/main/java/com/netcracker/utility/controller/FileController.java
@@ -4,15 +4,31 @@ import com.netcracker.utility.dto.UploadedLinks;
 import com.netcracker.utility.service.StorageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@Slf4j
-public class FileListingControllerV2 {
+public class FileController {
 
     private final StorageService storageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
+        if (!MediaType.APPLICATION_PDF.toString().equals(file.getContentType())) {
+            return ResponseEntity.badRequest().body("Only PDF file types are allowed");
+        }
+
+        storageService.upload(file);
+
+        return ResponseEntity.accepted().build();
+    }
 
     @GetMapping("/links")
     public UploadedLinks uploadedFiles() {
